6b32548ad9572a1d48cd4cd7e8ca5a11114bc159
heshuai <heshuai@topjoy.com>
2018-10-31 09:41:01 +0800

    Initial Commit

---
+++ learnOpenGl/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  learnOpenGl
+//
+//  Created by heshuai on 2018/10/31.
+//  Copyright © 2018年 heshuai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
